2f534d0908a090ae6fc658736f02437c7ba3a087
vintry <oennabati3@gmail.com>
2025-01-08 16:10:57 +0100

    commit

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+const MyFooter = () => {
+  return (
+    <footer className="bg-dark text-light text-center py-3">
+      <Container>
+        <p>© 2025 EpiBooks. All rights reserved.</p>
+      </Container>
+    </footer>
+  );
+};
+
+export default MyFooter;
